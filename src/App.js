7f3328d7d3b5f86a74cecaaf0d33be099229835d
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-05 19:28:41 +0200

    added modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <>
+      <Cart></Cart>
       <Header></Header>
       <main>
         <Meals />
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,30 @@
+import reactDom from 'react-dom';
+import classes from './Modal.module.css';
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <>
+      {reactDom.createPortal(<Backdrop></Backdrop>, portalElement)}
+      {reactDom.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
